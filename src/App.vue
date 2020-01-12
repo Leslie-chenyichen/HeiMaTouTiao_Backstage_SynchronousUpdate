6ef964b5c3457a1bb9006f940c3f2cd3d0a93719
一生挚爱(Leslie) <chenyichen8532522@163.com>
2020-01-12 15:50:42 +0800

    登录页面的最初页面完成
    下一步是改登录页面的样式

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
+<!-- 这个区域是【app挂载文件】 -->
+<!-- @/App.ue -->
   <div id="app">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'app'
+
 }
 </script>
 
-<style >
+<style lang="less">
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
+/* 【mian入口文件区域】 */
 // 引入Vue的框架
 import Vue from 'vue'
 // 挂载
 import App from './App.vue'
+// 引入路由
+import router from '@/router/index.js'
 // 引入框架
 import ElementUI from 'element-ui'
+// 引入面的样式
+import '@/styles/index.less'
 Vue.use(ElementUI)
+/*  */
 
 // 阻止启动生产消息，常用作指令。
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
