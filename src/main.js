aeff5ffc53951b689e84bcae96b509779974b225
一生挚爱(Leslie) <chenyichen8532522@163.com>
2020-01-12 14:53:43 +0800

    Vue_cli脚手架删除不相关的文件完成

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+// 引入Vue的框架
 import Vue from 'vue'
+// 挂载
 import App from './App.vue'
+// 引入框架
+import ElementUI from 'element-ui'
+Vue.use(ElementUI)
+
 // 阻止启动生产消息，常用作指令。
 Vue.config.productionTip = false
 
