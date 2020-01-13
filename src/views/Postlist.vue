ef172c0216714c628e2c8a3c31a56e6e7963fd36
一生挚爱(Leslie) <chenyichen8532522@163.com>
2020-01-13 14:11:44 +0800

    分页结构的添加和分页功能的实现

--- src/views/Postlist.vue
+++ src/views/Postlist.vue
@@ -11,8 +11,9 @@
       <el-table-column type="index" width="50"></el-table-column>
       <el-table-column prop="title" label="标题" width="800"></el-table-column>
       <el-table-column prop="name" label="日期" width="100"></el-table-column>
-      <el-table-column prop="type" label="类型"  width="100"></el-table-column>
-      <el-table-column prop="user.nickname" label="作者"  width="100"></el-table-column>
+      <el-table-column prop="type" label="类型" width="100"></el-table-column>
+      <template slot-scope="scope">{{scope.row.type===1?'文章':'视频'}}</template>
+      <el-table-column prop="user.nickname" label="作者" width="100"></el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
           <el-tooltip class="item" effect="dark" content="编辑" placement="top">
@@ -22,11 +23,21 @@
             <el-button type="success" icon="el-icon-share"></el-button>
           </el-tooltip>
           <el-tooltip class="item" effect="dark" content="删除" placement="top">
-           <el-button type="warning " icon="el-icon-delete"></el-button>
+            <el-button type="warning " icon="el-icon-delete"></el-button>
           </el-tooltip>
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页区域 -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pageIndex"
+      :page-sizes="[2, 4, 6, 8]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    ></el-pagination>
   </div>
 </template>
 
@@ -37,25 +48,35 @@ export default {
     return {
       postList: [],
       pageSize: 2,
-      pageIndex: 1
+      pageIndex: 1,
+      total: 0
     }
   },
   methods: {
-    edit (data) {
-      console.log(data)
+    handleSizeChange (val) {
+      console.log(`每页${val}条`)
+      this.pageSize = val
+      this.init()
+    },
+    // 切换时
+    handleCurrentChange (val) {
+      this.pageIndex = val
+      this.init()
+    },
+
+    // 获取文章列表的分页数据
+    async init () {
+      let res = await getPostList({
+        pageSize: this.pageSize,
+        pageIndex: this.pageIndex
+      })
+      this.postList = res.data.data
+      console.log(res)
     }
   },
-  // 获取文章列表的分页数据
-  async init () {
-    let res = await getPostList({ pageSize: this.pageSize, pageIndex: this.pageIndex })
-    this.postList = res.data.data
+  mounted () {
+    this.init()
   }
-  // async mounted () {
-  //   console.log(1)
-  //   let res = await getPostList()
-  //   console.log(res)
-  //   this.postList = res.data.data
-  // }
 }
 </script>
 
